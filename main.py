5956840c9bc0a4cbad4f13c2ebb6b4f35ac363ac
BuildTools <unconfigured@null.spigotmc.org>
2021-01-02 02:20:21 +0100

    fixed fScore and made graphic representation. Implemented interactive, graphic walls

--- main.py
+++ main.py
@@ -14,22 +14,23 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
 SCREEN_SIZE = 700
-BOARD_DIM = 5
+SIZE = (SCREEN_SIZE, SCREEN_SIZE)
+SCREEN = pg.display.set_mode(SIZE)
+BOARD_DIM = 10
 SQUARE_SIZE = SCREEN_SIZE / BOARD_DIM
 BOARD = np.arange(BOARD_DIM ** 2).reshape(BOARD_DIM, BOARD_DIM)
 WALL_SET = {1, 2, 3, 6, 7, 8, 12, 13, 18}
 
 
 def main():
+    print(BOARD)
+    print(a_star(0, 4))
     launch()
 
 
 def launch():
     carryOn = True
     clock = pg.time.Clock()
-
-    size = (SCREEN_SIZE, SCREEN_SIZE)
-    screen = pg.display.set_mode(size)
     pg.display.set_caption("AAAAASS")
 
     while carryOn:
@@ -37,39 +38,25 @@ def launch():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 carryOn = False
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                pos = pg.mouse.get_pos()
+                column = int((pos[0] // SQUARE_SIZE) - 1)
+                row = int((pos[1] // SQUARE_SIZE) - 1)
+                BOARD[row][column] = -1
 
-        cnt = 0
-        for i in range(BOARD_DIM + 1):
-            for j in range(BOARD_DIM + 1):
-                if cnt % 2 == 0:
-                    pg.draw.rect(
-                        screen,
-                        DARK_GREY,
-                        [
-                            SQUARE_SIZE * j,
-                            SQUARE_SIZE * i,
-                            SQUARE_SIZE,
-                            SQUARE_SIZE,
-                        ],
-                    )
-                else:
-                    pg.draw.rect(
-                        screen,
-                        LIGHT_GREY,
-                        [
-                            SQUARE_SIZE * j,
-                            SQUARE_SIZE * i,
-                            SQUARE_SIZE,
-                            SQUARE_SIZE,
-                        ],
-                    )
-                cnt += 1
-            cnt -= 1
+        grid_constructor()
 
         pg.display.flip()
         clock.tick(120)
 
     pg.quit()
+
+
+def grid_constructor():
+    if BOARD_DIM % 2 == 0:
+        box_color(BOARD_DIM)
+    else:
+        box_color(BOARD_DIM + 1)
 
 
 def reconstruct_path(cameFrom, current):
@@ -120,6 +107,8 @@ def neighbor_finder(current):
 
 
 def a_star(start, goal):
+    np.where(BOARD == start) = -2
+    np.where(BOARD == goal) = -3
     current = None
     openSet = set()
     openSet.add(start)
@@ -127,24 +116,18 @@ def a_star(start, goal):
     gScore = defaultdict(lambda: math.inf)
     gScore[start] = 0
 
-    # fScore = defaultdict(lambda: math.inf)
-    # fScore[start] = h(start, goal)
+    fScore = defaultdict(lambda: math.inf)
+    fScore[start] = h(start, goal)
 
     while len(openSet) != 0:
-
-        # for i in openSet:
-        #     temp_current = -1
-        #     if current is None:
-        #         current = start
-        #         break
-        #     if fScore[i] < fScore[temp_current]:
-        #         temp_current = i
-        #         current = i
-
-        if np.where(BOARD == start)[0][0] <= np.where(BOARD == goal)[0][0]:
-            current = min(openSet)
-        else:
-            current = max(openSet)
+        temp_current = -1
+        for i in openSet:
+            if current is None:
+                current = start
+                break
+            if fScore[i] < fScore[temp_current]:
+                temp_current = i
+                current = i
 
         if current == goal:
             return reconstruct_path(cameFrom, current)
@@ -158,7 +141,7 @@ def a_star(start, goal):
             if tentative_gScore < gScore[neighbor]:
                 cameFrom[neighbor] = current
                 gScore[neighbor] = tentative_gScore
-                # fScore[neighbor] = gScore[neighbor] + h(neighbor, goal)
+                fScore[neighbor] = gScore[neighbor] + h(neighbor, goal)
                 if neighbor not in openSet:
                     openSet.add(neighbor)
 
@@ -169,6 +152,56 @@ def wall_adder(num_set):
     for i in num_set:
         coord = (np.where(BOARD == i)[0][0], np.where(BOARD == i)[1][0])
         BOARD[coord[0]][coord[1]] = -1
+
+
+def box_color(second_range):
+    cnt = 0
+    for i in range(BOARD_DIM + 1):
+        for j in range(second_range):
+            if BOARD[i - 1][j - 1] == -1:
+                color = BLACK
+                pg.draw.rect(
+                    SCREEN,
+                    BLACK,
+                    [
+                        SQUARE_SIZE * j,
+                        SQUARE_SIZE * i,
+                        SQUARE_SIZE,
+                        SQUARE_SIZE,
+                    ],
+                )
+            elif BOARD[i - 1][j - 1] == -2:
+                color = RED
+            elif BOARD[i - 1][j - 1] == -3:
+                color = GREEN   
+            elif cnt % 2 == 0:
+                color = DARK_GREY
+                pg.draw.rect(
+                    SCREEN,
+                    DARK_GREY,
+                    [
+                        SQUARE_SIZE * j,
+                        SQUARE_SIZE * i,
+                        SQUARE_SIZE,
+                        SQUARE_SIZE,
+                    ],
+                )
+            else:
+                color = LIGHT_GREY
+                pg.draw.rect(
+                    
+                    SCREEN,
+                    LIGHT_GREY,
+                    [
+                        SQUARE_SIZE * j,
+                        SQUARE_SIZE * i,
+                        SQUARE_SIZE,
+                        SQUARE_SIZE,
+                    ],
+                )
+        
+            cnt += 1
+        cnt -= 1
 
 
 if __name__ == "__main__":
